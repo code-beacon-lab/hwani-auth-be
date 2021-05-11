d4b0c1b9e360b26a2ed193f7a61b5482a8e88071
danny <gryb809@gmail.com>
2021-05-11 16:37:03 +0900

    :tada: 모듈 생성

---
+++ server-auth/src/test/java/com/boiler/auth/ServerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boiler.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
